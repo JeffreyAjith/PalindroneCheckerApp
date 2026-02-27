84f1c95bca3a6f6ce96dc65d949a3bb470bc4794
JeffreyAjithKumar <jeffreyajith01@gmail.com>
2026-02-27 09:21:17 +0530

    Update UC3PalindroneCheckerApp.java

--- src/UC3PalindroneCheckerApp.java
+++ src/UC3PalindroneCheckerApp.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class UC3PalindroneCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+        if (original.equals(reversed)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+        sc.close();
+    }
 }
