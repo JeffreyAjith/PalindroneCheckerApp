de94b49058d1f152315fb455fb6fc7a0d414a597
JeffreyAjith <jeffreyajith01@gmail.com>
2026-02-27 09:28:37 +0530

    UC4 done

--- src/UC4PalindroneCheckerApp.java
+++ src/UC4PalindroneCheckerApp.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class UC4PalindroneCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        char[] characters = input.toCharArray();
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+        sc.close();
+    }
 }
